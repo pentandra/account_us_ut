d113c1ac540c136180d7c4b755b7b74490b8ec35
Chris Chapman <chris@pentandra.com>
2023-08-17 11:47:26 -0600

    Updated version and setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 
-from setuptools import setup
-import re
-import os
 import io
-try:
-    from configparser import ConfigParser
-except ImportError:
-    from ConfigParser import ConfigParser
+import os
+import re
+from configparser import ConfigParser
+
+from setuptools import find_packages, setup
 
 
 def read(fname):
-    return io.open(
+    content = io.open(
         os.path.join(os.path.dirname(__file__), fname),
         'r', encoding='utf-8').read()
+    content = re.sub(
+        r'(?m)^\.\. toctree::\r?\n((^$|^\s.*$)\r?\n)*', '', content)
+    return content
 
 
 def get_require_version(name):
@@ -27,8 +28,9 @@ def get_require_version(name):
         major_version, minor_version + 1)
     return require
 
+
 config = ConfigParser()
-config.readfp(open('tryton.cfg'))
+config.read_file(open(os.path.join(os.path.dirname(__file__), 'tryton.cfg')))
 info = dict(config.items('tryton'))
 for key in ('depends', 'extras_depend', 'xml'):
     if key in info:
@@ -39,29 +41,54 @@ major_version = int(major_version)
 minor_version = int(minor_version)
 name = 'trytond_account_us_ut'
 
-download_url = 'https://pypi.python.org/pypi/%s/%s' % (
-        name, version)
+download_url = 'http://downloads.tryton.org/%s.%s/' % (
+    major_version, minor_version)
+if minor_version % 2:
+    version = '%s.%s.dev0' % (major_version, minor_version)
+    download_url = (
+        'hg+http://hg.tryton.org/modules/%s#egg=%s-%s' % (
+            name[8:], name, version))
 
+local_version = []
+if os.environ.get('CI_JOB_ID'):
+    local_version.append(os.environ['CI_JOB_ID'])
+else:
+    for build in ['CI_BUILD_NUMBER', 'CI_JOB_NUMBER']:
+        if os.environ.get(build):
+            local_version.append(os.environ[build])
+        else:
+            local_version = []
+            break
+if local_version:
+    version += '+' + '.'.join(local_version)
 requires = []
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res)(\W|$)', dep):
         requires.append(get_require_version('trytond_%s' % dep))
 requires.append(get_require_version('trytond'))
 
+tests_require = []
+dependency_links = []
+if minor_version % 2:
+    dependency_links.append(
+        'https://trydevpi.tryton.org/?local_version='
+        + '.'.join(local_version)
+        )
+
 setup(name=name,
     version=version,
     description='Tryton module with United States chart of accounts (US GAAP)',
-    long_description=read('README'),
+    long_description=read('README.rst'),
     author='Pentandra',
     author_email='issues@pentandra.com',
     url='https://github.com/pentandra/account_us_ut',
     download_url=download_url,
-    keywords='tryton account chart us gaap united states english',
-    package_dir={'trytond.modules.account_us_ut': '.'},
-    packages=[
-        'trytond.modules.account_us_ut',
-        'trytond.modules.account_us_ut.tests',
-        ],
+    keywords='tryton account chart us gaap english',
+    package_dir={'trytond.modules.account_us': '.'},
+    packages=(
+        ['trytond.modules.account_us_ut']
+        + ['trytond.modules.account_us_ut.%s' % p for p in find_packages()]
+        ),
     package_data={
         'trytond.modules.account_us_ut': (info.get('xml', [])
             + ['tryton.cfg']),
@@ -76,23 +103,25 @@ setup(name=name,
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Office/Business',
         'Topic :: Office/Business :: Financial :: Accounting',
     ],
     license='GPL-3',
+    python_requires='>=3.7',
     install_requires=requires,
+    extras_require={
+        'test': tests_require,
+        },
+    dependency_links=dependency_links,
     zip_safe=False,
     entry_points="""
     [trytond.modules]
     account_us_ut = trytond.modules.account_us_ut
     """,
-    test_suite='tests',
-    test_loader='trytond.test_loader:Loader',
-    use_2to3=True,
     )
